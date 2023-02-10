42820e8ba1b84593b75ccdf2ec5210b7161017d5
Marco Sousa <marcosousacontato@gmail.com>
2023-02-10 15:11:58 -0300

    criando sellers controller

---
+++ SalesWebNet6/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebNet6.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
