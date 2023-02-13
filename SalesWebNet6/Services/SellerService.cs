5b6d514dba52a0ca7345512b08c4cb6a6c38475f
Marco Sousa <marcosousacontato@gmail.com>
2023-02-13 10:50:54 -0300

    Implementacao de Delete dos sellers

--- SalesWebNet6/Services/SellerService.cs
+++ SalesWebNet6/Services/SellerService.cs
@@ -22,5 +22,17 @@ namespace SalesWebNet6.Services
             _context.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById (int id)
+        {
+            return _context.Seller.FirstOrDefault(seller => seller.Id == id); 
+        }
+
+        public void Remove (int id) 
+        {
+            var seller = _context.Seller.Find(id); 
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
     }
 }
