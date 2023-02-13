1f35e9788f6ade0d5602521d11469b15bb53d2ae
Marco Sousa <marcosousacontato@gmail.com>
2023-02-13 14:41:57 -0300

    Implementando validação para o create e edit

--- SalesWebNet6/Controllers/SellersController.cs
+++ SalesWebNet6/Controllers/SellersController.cs
@@ -38,6 +38,12 @@ namespace SalesWebNet6.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if(!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Departments = departments, Seller = seller };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -105,7 +111,14 @@ namespace SalesWebNet6.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll(); 
+                var viewModel = new SellerFormViewModel { Departments = departments, Seller = seller };
+                return View(viewModel);
+            }
+
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { Message = "ID MISS MATCH" });
             }
